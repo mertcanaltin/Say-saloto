1ac9a530d0198d627c989279c01d0368e7bad9a6
Mert Can Altın <37827216+mertcanaltin@users.noreply.github.com>
2018-04-05 23:03:11 +0300

    Add files via upload

---
+++ sayisalloto/sayisalloto/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+
+    Private Sub OynaButon_Click(sender As Object, e As EventArgs) Handles OynaButon.Click
+
+        Dim sayilar(5) As Object
+
+        Dim rast As New Random
+
+        sayilar(0) = rast.Next(1, 50) '1 ile 49 arası sayılar üretmek için kullanır  
+        For i = 1 To sayilar.Count - 1
+            Dim sayi As Object
+            Do
+                sayi = rast.Next(1, 50)
+
+            Loop While sayilar.Contains(sayi) ' Loop Until sayilar.Contains(sayi)'
+            sayilar(i) = sayi
+
+        Next
+        Array.Sort(sayilar)
+        sayilarlistbox.Items.Clear()
+        sayilarlistbox.Items.AddRange(sayilar)
+
+        
+
+
+
+
+
+
+
+
+
+
+    End Sub
+End Class
